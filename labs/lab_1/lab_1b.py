bcad5ff7a339acc01803a300e7ebae8743ff15c2
aadisaraf <saraf.aadarsh@gmail.com>
2026-02-27 16:05:09 -0800

    added try & except block to prevent error

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -47,8 +47,11 @@ def main():
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
-    result = simple_calculator(operation, num1, num2)
-    print(f"The result of {operation}ing {num1} and {num2} is: {result}")
+    try:
+        result = simple_calculator(operation, num1, num2)
+        print(f"The result of {operation}ing {num1} and {num2} is: {result}")
+    except ValueError as error:
+        print(error)
 
 
 if __name__ == "__main__":
